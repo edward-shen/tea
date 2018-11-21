58271b57833663143b62ba993dc66c309d1fc5b4
Edward Shen <6173958+edward-shen@users.noreply.github.com>
2018-11-21 01:56:44 -0500

    added pdf parsing. added a list of a question ids used for correlating pdf values to their stats

---
+++ src/parsers/pdf.ts
@@ -0,0 +1,75 @@
+import { readFileSync } from 'fs';
+import * as pdf from 'pdf-parse';
+
+import ids from './questionIDs';
+
+interface Summary {
+  mean: number;
+  deptMean: number;
+  univMean: number;
+  median: number;
+  deptMedian: number;
+  univMedian: number;
+  stdev: number;
+}
+
+interface QuestionMeta {
+  courseMean: number;
+  deptMean: number;
+  univMean: number;
+}
+
+/**
+ * Structures data from a PDF file. Returns an object containing a mapping from
+ * a quesiton ID to the parsed variant. It also contains overall central
+ * tendencies.
+ *
+ * In many ways, due to how we're scraping data from a pdf file, this is very
+ * brittle. If NEU decides to change their questions, then this will likely
+ * fail.
+ *
+ * @param stream A binary pdf file.
+ */
+async function parse(stream) {
+  const shit = readFileSync('/home/edward/Downloads/pdf.pdf');
+  const pdfData = (await pdf(shit)).text;
+  // The magic of regex <3
+  const matched = pdfData.match(/(?<=%)[.\d]+/g);
+
+  const summaryGenerator = [
+    'courseSum',
+    'learningSum',
+    'instructorSum',
+    'effectivenessSum',
+  ][Symbol.iterator]();
+  const questionGenerator = ids[Symbol.iterator]();
+
+  const summaryKeys = ['mean', 'deptMean', 'univMean', 'median',
+  'deptMedian', 'univMedian', 'stdev'];
+  const questionKeys = ['courseMean', 'deptMean', 'univMean'];
+
+  const ret = {};
+  matched.map((str) => {
+    const row = [];
+    for (let i = 0; i < str.length; i += 3) {
+      row.push(Number(str.slice(i, i + 3)));
+    }
+    if (row.length === 7) {
+      ret[summaryGenerator.next().value] = handleRow(summaryKeys, row);
+    } else {
+      ret[questionGenerator.next().value] = handleRow(questionKeys, row);
+    }
+  });
+
+  return ret;
+}
+
+function handleRow(keys: string[], row) {
+  const ret = {};
+  row.map((v, i) => ret[keys[i]] = v);
+  return ret;
+}
+
+export {
+  parse,
+};
---
+++ src/parsers/questionIDs.ts
@@ -0,0 +1,41 @@
+
+/**
+ * These values were generated from manually downloading a excel sheet and
+ * copying those values, since there is a 1-1 correlation between the pdf
+ * questions and excel questions.
+ */
+const values = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  348,
+  350,
+  351,
+  352,
+  8,
+  10,
+  11,
+  12,
+  13,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  21,
+  22,
+  23,
+  24,
+  25,
+  26,
+  27,
+  87,
+  9,
+];
+
+export default values;
