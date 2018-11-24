457a7ce964e264108abc993eddd464fccfa431ef
Edward Shen <6173958+edward-shen@users.noreply.github.com>
2018-11-24 00:56:41 -0500

    Don't updated Class Cache if fully updated.

--- src/index.ts
+++ src/index.ts
@@ -18,62 +18,66 @@ async function main() {
   const driver = new Driver(username, password);
   await driver.auth();
 
+  const numClasses = await driver.latestSize();
   if (await driver.checkCache() === CacheStatus.OUT_OF_DATE) {
-    await metacache.updateCache(driver, await driver.latestSize());
+    await metacache.updateCache(driver, numClasses);
   }
 
   const meta = await metacache.getReportData();
   const pool = new RequestPool();
-  console.log('Generating class db');
+  if (await ClassCache.size() === numClasses) {
+    console.log('Class DB up to date!');
+  } else {
+    console.log('Class DB not up to date, updating!');
+    const bar = new Bar({}, Presets.shades_classic);
+    bar.start(Object.values(meta).length, 0);
 
-  const bar = new Bar({}, Presets.shades_classic);
-  bar.start(Object.values(meta).length, 0);
+    for (const data of Object.values(meta)) {
+      // These must be blocking, and must be located here to avoid the race
+      // condition where all iterations are waiting for a request to get pdf data.
+      // In other words, this operation must be atomic.
+      await pool.request();
+      await pool.request();
+      driver.getExcel(data.id, data.instructorId, data.termId).then((rawExcel) => {
+        const [excel, responses, declines] = parseExcel(rawExcel);
+        driver.getPdf(data.id, data.instructorId, data.termId).then(async (rawPdf) => {
+          // Once both requests have been completed, return the request to the
+          // pool as fast as possible
+          pool.return();
+          pool.return();
+          let pdf = await parsePdf(rawPdf);
+          if (!pdf) {
+            console.log(`PDF Parsing failed for ${data.id}, ${data.instructorId}, ${data.termId}`);
+            pdf = {} as PDFData;
+          }
 
-  for (const data of Object.values(meta)) {
-    // These must be blocking, and must be located here to avoid the race
-    // condition where all iterations are waiting for a request to get pdf data.
-    // In other words, this operation must be atomic.
-    await pool.request();
-    await pool.request();
-    driver.getExcel(data.id, data.instructorId, data.termId).then((rawExcel) => {
-      const [excel, responses, declines] = parseExcel(rawExcel);
-      driver.getPdf(data.id, data.instructorId, data.termId).then(async (rawPdf) => {
-        // Once both requests have been completed, return the request to the
-        // pool as fast as possible
-        pool.return();
-        pool.return();
-        let pdf = await parsePdf(rawPdf);
-        if (!pdf) {
-          console.log(`PDF Parsing failed for ${data.id}, ${data.instructorId}, ${data.termId}`);
-          pdf = {} as PDFData;
-        }
+          for (const question of excel) {
+            const id = question.id;
+            delete question.id;
+            pdf[id] = {
+              ...pdf[question.id],
+              ...question,
+            };
+          }
 
-        for (const question of excel) {
-          const id = question.id;
-          delete question.id;
-          pdf[id] = {
-            ...pdf[question.id],
-            ...question,
+          const toAdd = {
+            ...pdf,
+            responses,
+            declines,
           };
-        }
 
-        const toAdd = {
-          ...pdf,
-          responses,
-          declines,
-        };
-
-        ClassCache.put(data.id, toAdd);
+          ClassCache.put(data.id, toAdd);
+        });
       });
-    });
 
-    bar.increment(1);
+      bar.increment(1);
+    }
+
+    await pool.barrier();
+
+    bar.stop();
+    console.log('Class Cache generated!');
   }
-
-  await pool.barrier();
-
-  bar.stop();
-  console.log('class cache generated!');
 }
 
 main();
