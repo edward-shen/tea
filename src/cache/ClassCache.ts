8e300f8422d75763d008330174aa89d959c336e1
Edward Shen <6173958+edward-shen@users.noreply.github.com>
2018-11-24 00:20:22 -0500

    added way to get class cache size

--- src/cache/ClassCache.ts
+++ src/cache/ClassCache.ts
@@ -12,6 +12,32 @@ class ClassCache {
     this.db = levelup(leveldown(resolve(__dirname, '../../cache/classdb')));
   }
 
+  public async size() {
+    let count = 0;
+    let isDone = false;
+    const iterator = this.db.iterator();
+
+    while (!isDone) {
+      await new Promise((resolve, reject) => {
+        iterator.next((err, key, value) => {
+          if (err) {
+            reject();
+          }
+
+          if (key && value) {
+            count += 1;
+          } else {
+            isDone = true;
+          }
+
+          resolve();
+        });
+      });
+    }
+
+    return count;
+  }
+
   public async put(key: string, value) {
     await this.db.put(key, JSON.stringify(value));
   }
