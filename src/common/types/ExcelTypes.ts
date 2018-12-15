11da8daceef8e94c4f71f1f1582e5d9f6991d855
Edward Shen <code@eddie.sh>
2018-12-14 20:27:14 -0500

    documentation on types

--- src/common/types/ExcelTypes.ts
+++ src/common/types/ExcelTypes.ts
@@ -1,3 +1,16 @@
+/**
+ * This class contains all type declarations for the Excel scraper. This file
+ * acts as the API definitions between the Excel part of the scraper and
+ * dependents on its data.
+ *
+ * When post-scraping processes are implemented, this definition should change,
+ * and internal Excel types should be handled in the Excel parsing class.
+ */
+
+ /**
+  * All questions scraped from the Excel spreadsheet will contain the following
+  * fields.
+  */
 interface Base {
   id: number;
   abbrev: string;
@@ -5,6 +18,11 @@ interface Base {
   respCount: number;
 }
 
+/**
+ * Declaration of fields for a TRACE question that does not have an NA Field.
+ * Surprisingly, as of writing this, there exists only one question that is
+ * applicable to this interface.
+ */
 interface Question extends Base {
   5: number;
   4: number;
@@ -17,10 +35,18 @@ interface Question extends Base {
   respRate: number;
 }
 
+/**
+ * A TRACE question with an "Not applicable" field. Most questions extracted
+ * from the Excel sheet will contain this field.
+ */
 interface NAQuestion extends Question {
   '-1': number;
 }
 
+/**
+ * Special interface for the "hours worked" question, as it's formatted as a
+ * demographic question, rather than a standard question.
+ */
 interface HoursQuestion extends Base {
   '17-20': number;
   '13-16': number;
--- src/common/types/PDFTypes.ts
+++ src/common/types/PDFTypes.ts
@@ -1,4 +1,18 @@
+/**
+ * This class contains all type declarations for the PDF scraper. This file acts
+ * as the API definitions between the PDF part of the scraper and dependents on
+ * its data.
+ *
+ * When post-scraping processes are implemented, this definition should change,
+ * and internal PDF types should be handled in the PDF parsing class.
+ */
 
+ /**
+  * Central tendencies of the summaries scraped from the PDF. There should only
+  * be four of these objects: a course sum, learning sum, instructor sum, and
+  * effectiveness sum. All values are relative to the term, and are not all time
+  * values.
+  */
 interface PDFSummary {
   mean: number;
   deptMean: number;
@@ -9,12 +23,19 @@ interface PDFSummary {
   stdev: number;
 }
 
+/**
+ * The central tendencies extracted from the PDF for a single question. All
+ * values are relative to the term, and are not all time values.
+ */
 interface PDFQuestion {
   courseMean: number;
   deptMean: number;
   univMean: number;
 }
 
+/**
+ * POD Object declaration for what the PDF scraper will return.
+ */
 interface PDFData {
   courseSum: PDFSummary;
   learningSum: PDFSummary;
