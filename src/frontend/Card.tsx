7dc7d19f04b42428f913f69b125233cb51cec675
Edward Shen <code@eddie.sh>
2018-12-16 12:15:22 -0500

    Updated card to be fully generic

--- src/frontend/Card.tsx
+++ src/frontend/Card.tsx
@@ -7,21 +7,35 @@ import './css/Card.scss';
 
 class Card extends React.Component<CardType> {
   public render() {
+    let header = (
+      <header>
+        <h2>{this.props.header}</h2>
+        <h3>{this.props.subheader}</h3>
+      </header>);
+    let left = <em>{this.props.leftText}</em>;
+    let right = <p>{this.props.rightText}</p>;
+
+    if (this.props.bodyLink) {
+      header = (
+        <Link className='card-top' to={this.props.bodyLink}>
+          {header}
+        </Link>);
+    }
+
+    if (this.props.leftTextLink) {
+      left = <Link to={this.props.leftTextLink}>{left}</Link>;
+    }
+
+    if (this.props.rightTextLink) {
+      right = <Link to={this.props.rightTextLink}>{right}</Link>;
+    }
+
     return (
       <section className='card hover-shadow rounded'>
-        <Link className='card-top' to={`/report/${this.props.bodyLink}`}>
-          <header>
-            <h2>{this.props.header}</h2>
-            <h3>{this.props.subheader}</h3>
-          </header>
-        </Link>
+        {header}
         <footer>
-          <Link to={this.props.leftTextLink}>
-            <em>{this.props.leftText}</em>
-          </Link>
-          <Link to={`/prof/`}>
-            <p>{this.props.rightText}</p>
-          </Link>
+          {this.props.leftText && left}
+          {this.props.rightText && right}
         </footer>
       </section>
     );
