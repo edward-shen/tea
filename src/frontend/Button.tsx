9f8589cd7d4e296b4265de8810e8df6870e43229
Edward Shen <code@eddie.sh>
2018-12-16 13:31:15 -0500

    Button now can accept inner react component instead

--- src/frontend/Button.tsx
+++ src/frontend/Button.tsx
@@ -3,17 +3,18 @@ import { Link } from 'react-router-dom';
 
 interface ButtonProps {
   link: string;
-  text: string;
+  text?: string;
+  inner?: React.Component;
 }
 
 class Button extends React.Component<ButtonProps> {
   public render() {
     return (
       <Link to={this.props.link}>
-      <div className='hover-shadow rounded button'>
-        <p>{this.props.text}</p>
-      </div>
-    </Link>);
+        <div className='hover-shadow rounded button'>
+          {this.props.inner || <p>{this.props.text}</p>}
+        </div>
+      </Link>);
   }
 }
 
