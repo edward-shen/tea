ab1076abe9fa986d7e927d517b4b706dc0986342
Edward Shen <code@eddie.sh>
2018-12-17 00:12:50 -0500

    Added footer

---
+++ src/frontend/Footer.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+class Footer extends React.Component<{}> {
+  public render() {
+    return (
+      <footer className='footer'>
+        <p>Made with love and a lot of tea.</p>
+      </footer>);
+  }
+}
+
+export default Footer;
