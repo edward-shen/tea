dea800748742189e40df055a6002cf3808d3bbbe
Edward Shen <code@eddie.sh>
2018-12-17 12:47:57 -0500

    Fixed a bug where cardview skipped the first page

--- src/frontend/views/CardView.tsx
+++ src/frontend/views/CardView.tsx
@@ -22,7 +22,7 @@ class CardView extends React.Component<{}, CardViewState> {
       <InfiniteScroller
         className='cardview'
         hasMore={true}
-        pageStart={0}
+        pageStart={-1} // ??? InfiniteScroller dev wtf you doing, this needs to be -1 to start at 0
         loader={<div className='loader' key={0}>Loading ...</div>}
         loadMore={this.getMore.bind(this)}>
         {this.state.results}
