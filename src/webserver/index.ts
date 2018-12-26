17b2bbad24793d00a9f4218ea3ea1d4a1a8cac4d
Edward Shen <code@eddie.sh>
2018-12-25 21:27:20 -0500

    chore(legacy): removed REST api endpoint for reports

--- src/webserver/index.ts
+++ src/webserver/index.ts
@@ -30,33 +30,6 @@ app.get('/api/search', async (req, res) => {
     .then(results => res.send(toReportCard(results)));
 });
 
-app.get('/api/report', async (req, res) => {
-  mongoClient.get({ id: Number(req.query.id), instructorId: Number(req.query.prof) })
-    .then((result) => {
-      if (result.length === 1) {
-        res.send(result[0]);
-      } else {
-        const difference = Object.keys(diff(result[0], result[1]));
-        if (difference.length === 1 && difference[0] === '_id') {
-          console.warn('Multiple results found for id/prof combo; corrupted metadata db?');
-          res.send(result[0]);
-        } else {
-          console.log(result.map((a) => {
-            return {
-              _id: a._id,
-              queryId: req.query.id,
-              instructorId: req.query.prof,
-              instructorFirstName: a.instructorFirstName,
-              subject: a.subject,
-              number: a.number,
-            };
-          }));
-          res.send({ error: result.length });
-        }
-      }
-    });
-});
-
 app.listen(Config.dev_server.express_port, () => {
   console.log(`Server listening on port ${Config.dev_server.express_port}${server.graphqlPath}`);
 });
